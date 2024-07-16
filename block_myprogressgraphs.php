cee53a8fa0cc7599bf563557178484368c5c0bc2
lewiscarr79 <125464132+lewiscarr79@users.noreply.github.com>
2024-07-16 09:03:12 +0100

    Fix to allow for zero data
    Added a fix to add logic for empty data

--- block_myprogressgraphs.php
+++ block_myprogressgraphs.php
@@ -30,7 +30,7 @@ class block_myprogressgraphs extends block_base {
             FROM {user_lastaccess}
             WHERE timeaccess >= :timeaccess
         ", array('timeaccess' => time() - (30 * 24 * 60 * 60)));
-        $graph1_percent = round(($users_logged_in_30days / $active_users) * 100, 2);
+        $graph1_percent = $active_users > 0 ? round(($users_logged_in_30days / $active_users) * 100, 2) : 0;
 
         // Graph 2: New users added
         $total_users = $DB->count_records('user', array('deleted' => 0));
@@ -39,7 +39,7 @@ class block_myprogressgraphs extends block_base {
             FROM {user}
             WHERE firstaccess >= :firstaccess
         ", array('firstaccess' => time() - (30 * 24 * 60 * 60)));
-        $graph2_percent = round(($new_users_added / $total_users) * 100, 2);
+        $graph2_percent = $total_users > 0 ? round(($new_users_added / $total_users) * 100, 2) : 0;
 
         // Graph 3: Quiz completions vs attempts
         $quiz_completions = $DB->count_records_sql("
@@ -53,7 +53,7 @@ class block_myprogressgraphs extends block_base {
             FROM {quiz_attempts}
             WHERE timefinish >= :timefinish
         ", array('timefinish' => time() - (30 * 24 * 60 * 60)));
-        $graph3_percent = round(($quiz_completions / $quiz_attempts) * 100, 2);
+        $graph3_percent = $quiz_attempts > 0 ? round(($quiz_completions / $quiz_attempts) * 100, 2) : 0;
 
         // Graph 4: Course access
         $total_courses = $DB->count_records('course', array('visible' => 1));
@@ -62,46 +62,43 @@ class block_myprogressgraphs extends block_base {
             FROM {user_lastaccess}
             WHERE timeaccess >= :timeaccess
         ", array('timeaccess' => time() - (30 * 24 * 60 * 60)));
-        $graph4_percent = round(($courses_accessed / $total_courses) * 100, 2);
+        $graph4_percent = $total_courses > 0 ? round(($courses_accessed / $total_courses) * 100, 2) : 0;
+
+        $graph1_html = $this->get_graph_html($graph1_percent, 'graph1_title', 'graph1_description');
+        $graph2_html = $this->get_graph_html($graph2_percent, 'graph2_title', 'graph2_description');
+        $graph3_html = $this->get_graph_html($graph3_percent, 'graph3_title', 'graph3_description');
+        $graph4_html = $this->get_graph_html($graph4_percent, 'graph4_title', 'graph4_description');
 
         $output = "
             <div class='progress-graphs'>
-                <div class='progress-graph'>
-                <hr>
-                    <h6>" . get_string('graph1_title', 'block_myprogressgraphs') . "</h6>
-                    <p class='graph-description'>" . get_string('graph1_description', 'block_myprogressgraphs') . "</p>
-                    <div class='progress'>
-                        <div class='progress-bar' role='progressbar' style='width: {$graph1_percent}%; background-color: " . $this->get_progress_bar_color($graph1_percent) . ";' aria-valuenow='{$graph1_percent}' aria-valuemin='0' aria-valuemax='100'>{$graph1_percent}%</div>
-                    </div>
-                </div>
-                <div class='progress-graph'>
-                 <hr>
-                    <h6>" . get_string('graph2_title', 'block_myprogressgraphs') . "</h6>
-                    <p class='graph-description'>" . get_string('graph2_description', 'block_myprogressgraphs') . "</p>
-                    <div class='progress'>
-                        <div class='progress-bar' role='progressbar' style='width: {$graph2_percent}%; background-color: " . $this->get_progress_bar_color($graph2_percent) . ";' aria-valuenow='{$graph2_percent}' aria-valuemin='0' aria-valuemax='100'>{$graph2_percent}%</div>
-                    </div>
-                </div>
-                <div class='progress-graph'>
-                 <hr>
-                    <h6>" . get_string('graph3_title', 'block_myprogressgraphs') . "</h6>
-                    <p class='graph-description'>" . get_string('graph3_description', 'block_myprogressgraphs') . "</p>
-                    <div class='progress'>
-                        <div class='progress-bar' role='progressbar' style='width: {$graph3_percent}%; background-color: " . $this->get_progress_bar_color($graph3_percent) . ";' aria-valuenow='{$graph3_percent}' aria-valuemin='0' aria-valuemax='100'>{$graph3_percent}%</div>
-                    </div>
-                </div>
-                <div class='progress-graph'>
-                 <hr>
-                    <h6>" . get_string('graph4_title', 'block_myprogressgraphs') . "</h6>
-                    <p class='graph-description'>" . get_string('graph4_description', 'block_myprogressgraphs') . "</p>
-                    <div class='progress'>
-                        <div class='progress-bar' role='progressbar' style='width: {$graph4_percent}%; background-color: " . $this->get_progress_bar_color($graph4_percent) . ";' aria-valuenow='{$graph4_percent}' aria-valuemin='0' aria-valuemax='100'>{$graph4_percent}%</div>
-                    </div>
-                </div>
+                $graph1_html
+                $graph2_html
+                $graph3_html
+                $graph4_html
             </div>
         ";
 
         return $output;
+    }
+
+    private function get_graph_html($percent, $title_string, $description_string) {
+        $title = get_string($title_string, 'block_myprogressgraphs');
+        $description = get_string($description_string, 'block_myprogressgraphs');
+        
+        $graph_content = $percent > 0 ?
+            "<div class='progress'>
+                <div class='progress-bar' role='progressbar' style='width: {$percent}%; background-color: " . $this->get_progress_bar_color($percent) . ";' aria-valuenow='{$percent}' aria-valuemin='0' aria-valuemax='100'>{$percent}%</div>
+            </div>" :
+            "<p>No data available</p>";
+
+        return "
+            <div class='progress-graph'>
+                <hr>
+                <h6>{$title}</h6>
+                <p class='graph-description'>{$description}</p>
+                {$graph_content}
+            </div>
+        ";
     }
 
     private function get_progress_bar_color($percent) {
@@ -114,4 +111,3 @@ class block_myprogressgraphs extends block_base {
         }
     }
 }
-?>
